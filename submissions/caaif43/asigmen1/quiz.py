fc524b1df786245a823fe35d2907147c561d5a3f
caaif43 <iidleyare775@gmail.com>
2026-05-05 13:42:26 +0300

    assigmen1

---
+++ submissions/caaif43/asigmen1/quiz.py
@@ -0,0 +1,46 @@
+# abdulaahi
+#barnimijka waa kedis ku saabsan aqoon guud
+
+score = 0
+name= input("fadlan kali magacaaga =")
+print("ku soo dhawow kediska = "+name)
+
+#question 1
+print("waa kuma madexweynihii ugu horeyay somali?")
+answers1= input("jawaabta = ")
+if answers1.lower() == "adan abdulle" :
+     print("waa sax")
+     score =+1
+     print(f"hamblyo{name}, {score} point.")
+    
+else: 
+    print("waa khalad")
+    #question 2
+print("waa maxay casimada somalia?")
+answers2 = input("jawaato=")
+if answers2.lower() == "muqdisho":
+      print("waa sax")
+      score +=1
+      print(f"hamblyo{name}, {score} point.")
+else:
+      print("waa khalad")
+    #question 3
+print("qarada ugu weyn dunida waa tee?")
+answers2 = input("jawaato=")
+if answers2.lower() == "asia":
+      print("waa sax")
+      score +=1
+      print(f"hamblyo{name}, {score} point.")
+else:
+      print("waa khalad")
+ 
+      #question4
+print("dalka ugu dadka badan dunida waa?")
+answers2 = input("jawaato=")
+if answers2.lower() == "india":
+      print("waa sax")
+      score +=1
+      print(f"hamblyo{name}, {score} point.")
+else:
+      print("waa khalad")
+   
